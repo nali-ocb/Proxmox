4273d62c85349e4a130323f6cdbb0be6b1fc315c
NAli <104458796+nali-ocb@users.noreply.github.com>
2023-03-06 23:47:08 +0530

    update

--- ct/photoprism-v5.sh
+++ ct/photoprism-v5.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2021-2023 tteck
-# Author: tteck (tteckster)
+# Copyright (c) 2021-2023  NAli
+# Author: NAli
 # License: MIT
-# https://github.com/tteck/Proxmox/raw/main/LICENSE
+# https://github.com/nali-ocb/Proxmox/raw/main/LICENSE
 
 function header_info {
 clear
@@ -401,15 +401,15 @@ export PCT_OPTIONS="
   -unprivileged $CT_TYPE
   $PW
 "
-bash -c "$(wget -qLO - https://raw.githubusercontent.com/tteck/Proxmox/main/ct/create_lxc.sh)" || exit
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/nali-ocb/Proxmox/main/ct/create_lxc.sh)" || exit
 msg_info "Starting LXC Container"
 pct start $CTID
 msg_ok "Started LXC Container"
-lxc-attach -n $CTID -- bash -c "$(wget -qLO - https://raw.githubusercontent.com/tteck/Proxmox/main/install/$var_install.sh)" || exit
+lxc-attach -n $CTID -- bash -c "$(wget -qLO - https://raw.githubusercontent.com/nali-ocb/Proxmox/main/install/$var_install.sh)" || exit
 IP=$(pct exec $CTID ip a s dev eth0 | awk '/inet / {print $2}' | cut -d/ -f1)
 pct set $CTID -description "# ${APP} LXC
-### https://tteck.github.io/Proxmox/
-<a href='https://ko-fi.com/D1D7EP4GF'><img src='https://img.shields.io/badge/☕-Buy me a coffee-red' /></a>"
+### https://nali.github.io/Proxmox/
+<a href='NAli'' /></a>"
 msg_info "Setting Container to Normal Resources"
 pct set $CTID -memory 2048
 msg_ok "Set Container to Normal Resources"
